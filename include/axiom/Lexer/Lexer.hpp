1b5aac75faa69be534361604a5fcd04c1d6904c7
killian <delvalvalletk@gmail.com>
2026-09-13 19:23:38 +0200

    Implementation du squelette des classes utiles au lexer, classe Lexer et Token

---
+++ include/axiom/Lexer/Lexer.hpp
@@ -0,0 +1,20 @@
+// Ce fichier ne doit être inclus qu'une seule fois à la compilation
+#pragma once
+
+/* 
+    classe de std permettant de faire une vue vers un string,
+    une référence vers son emplacement mémoire
+*/
+
+#include <string_view>
+
+// classe de génération de token à réaliser
+#include "Token.hpp"
+
+namespace axiom{
+    class Lexer{
+        ;
+    }
+    ;
+}
+
