96a90443cafb1085fa66c23f8d1edff5e159f121
killian <delvalvalletk@gmail.com>
2026-09-17 19:03:05 +0200

    Change la forme de Token.hpp afin de contenir la valeur du token

--- include/axiom/Lexer/Token.hpp
+++ include/axiom/Lexer/Token.hpp
@@ -1,7 +1,4 @@
 #pragma once
-#include <string_view>
-
-using namespace std;
 
 namespace axiom{
     class Token{
@@ -20,7 +17,7 @@ namespace axiom{
             explicit Token(Type type, std::string_view value);
 
         private:
-            Type type_;
+            Type type_; 
             std::string_view value_; 
     };
 }
